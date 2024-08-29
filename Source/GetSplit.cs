ec4cfaf6165124bc463a33e05a4b20a067b20db9
nozwock <57829219+nozwock@users.noreply.github.com>
2024-08-29 14:18:18 +0530

    Initial commit

---
+++ Source/GetSplit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Xml;
+using Verse;
+
+namespace XmlGetSplit
+{
+    public class GetSplit : PatchOperationPathed
+    {
+        public string sep = "~";
+        public string storeTo = "";
+
+        public static void Log(string s)
+        {
+            Verse.Log.Message($"[XmlGetSplit] {s}");
+        }
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            XmlNode? node;
+            string? value;
+
+            node = xml.SelectSingleNode(storeTo);
+            value = xml.SelectSingleNode(xpath).InnerText;
+
+            node.RemoveAll();
+
+            foreach (var s in value.Split(new[] { sep }, StringSplitOptions.None))
+            {
+                var liNode = xml.CreateElement("li");
+                liNode.InnerText = s;
+                node.AppendChild(liNode);
+            }
+
+            return true;
+        }
+    }
+}
